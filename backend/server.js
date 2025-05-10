00ccb7d764ef9b9634c7e3e98c2f99078fe3e103
Abdalla881 <osabdalla881@gmail.com>
2025-05-10 23:49:16 +0300

    add vercel

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
